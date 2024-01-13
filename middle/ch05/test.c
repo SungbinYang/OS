3a52acbee5da5372862d62d3868eb057742cd178
SungbinYang <ysb05222@naver.com>
2024-01-13 11:50:32 +0900

    컴파일과 프로세스

---
+++ middle/ch05/test.c
@@ -0,0 +1,13 @@
+// 컴파일 과정 예시
+#include <stdio.h>
+
+#define MY_NUMBER 100
+
+int main(void) {
+  int num1 = 50;
+
+  printf("Result: %d", num1 + MY_NUMBER); // num1과 MY NUNBER를 더한 값 출력
+
+  getChar();
+  return 0;
+}
